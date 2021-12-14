9708b2d28d0afe49987de0aabd189cd52e9bd01e
Enrico Ciciriello <Hacryn@users.noreply.github.com>
2021-12-14 15:11:42 +0100

    Setup project

---
+++ client/src/Esonero Reti UPD (Client).c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Esonero.c
+ Author      : Enrico Ciciriello -  Simone Summo
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!"); /* prints Hello World! */
+	return EXIT_SUCCESS;
+}
